91a4bc571c8cd584bd04280776fe9ee6973cfc45
DESKTOP-9KPQD1P\정승훈 <jungsh2328@naver.com>
2022-05-24 20:10:43 +0900

    search + add

--- style-guide/style-guide/style-guide/src/main/java/com/graduation/styleguide/repository/UploadRepository.java
+++ style-guide/style-guide/style-guide/src/main/java/com/graduation/styleguide/repository/UploadRepository.java
@@ -1,11 +1,16 @@
 package com.graduation.styleguide.repository;
 
 import com.graduation.styleguide.domain.UploadInfo;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UploadRepository extends JpaRepository<UploadInfo, Long> {
 //    UploadInfo findByName(String product_name);
+    @Query(value = "SELECT * FROM clothes WHERE product_name LIKE :product_name OR product_name LIKE CONCAT('%,',:product_name,',%') OR product_name LIKE CONCAT('%,',:product_name) " + "OR product_name LIKE CONCAT(:product_name,',%')", nativeQuery = true)
+    List<UploadInfo> searchResult(String product_name, Pageable list);
+
 }
